d4f95633e4755c165d82e742fbfe7a70f7c3e788
jahir-ag <jahir.agudo.dev@gmail.com>
2025-07-31 01:04:34 -0500

    Prueba 12

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,18 @@ const allowedOrigins = [
 ]; */
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origen no permitido por CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
+app.use(express.json());
 
 // Importar rutas
 const authRoutes = require('./src/routes/authRoutes');
